2fa54090dd1249bb104ebc08d466b29b9f3b6519
kipdurron <ishunin97@gmail.com>
2021-02-19 18:56:01 +0300

    Initial Commit

---
+++ AIOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AIOS
+//
+//  Created by Илья Кадыров on 19.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
